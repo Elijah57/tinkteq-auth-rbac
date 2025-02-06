385a29a248bfca52b708d4ed9d3580d36f36c32e
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-02-06 11:40:19 +0100

    dev: fixes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllUsers } from "../controllers/adminController";
+import { isAdmin, isLoggedIn } from "../middlewares/authMiddleware";
 
 const adminRouter = Router();
 
-adminRouter.get("/users", getAllUsers);
+adminRouter.get("/users", isLoggedIn, isAdmin, getAllUsers);
 
 
 export default adminRouter
